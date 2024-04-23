086b73b9fc8be4c0eac8eb0e3231a3836a7482f0
Rod97139 <fayaflame@gmail.com>
2024-04-23 11:27:52 +0200

    login logout ok

--- lib/src/modules/auth/blocs/sign_in_bloc/sign_in_state.dart
+++ lib/src/modules/auth/blocs/sign_in_bloc/sign_in_state.dart
@@ -12,3 +12,4 @@ final class SignInInitial extends SignInState {}
 final class SignInSuccess extends SignInState {}
 final class SignInProcess extends SignInState {}
 final class SignInFailure extends SignInState {}
+final class SignOutSuccess extends SignInState {}
--- lib/src/modules/base/views/base_screen.dart
+++ lib/src/modules/base/views/base_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:pizza_admin/src/modules/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'dart:html' as html;
 class BaseScreen extends StatefulWidget {
   final Widget child;
   const BaseScreen(this.child, {super.key});
@@ -14,84 +15,92 @@ class BaseScreen extends StatefulWidget {
 class _BaseScreenState extends State<BaseScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: kToolbarHeight,
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          context.go('/home');
-                        },
-                        child: const Text(
-                          'Pizza Admin',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,),
+    return BlocListener<SignInBloc, SignInState>(
+      listener: (context, state) {
+        if (state is SignOutSuccess) {
+          html.window.location.reload();
+        }
+      
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: kToolbarHeight,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
                         InkWell(
                           onTap: () {
-                            //redirect to
-                            context.go('/create');
+                            context.go('/home');
                           },
                           child: const Text(
-                            'Create Pizza',
+                            'Pizza Admin',
                             style: TextStyle(
-                              // fontSize: 20,
-                              color: Colors.grey,
-                              // fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
-                    ],
-                  ),
-                  InkWell(
-                          onTap: () {
-                            //onclick do this:
-                            context.read<SignInBloc>().add(SignOutRequired());
-                          },
-                          splashColor: Colors.red,
-                          child: const Row(
-                            children: [
-                              Text(
-                                'Logout',
-                                style: TextStyle(
-                                  // fontSize: 20,
-                                  // color: Colors.grey,
-                                  // fontWeight: FontWeight.w600,
-                                ),
+                        const SizedBox(
+                          width: 20,),
+                          InkWell(
+                            onTap: () {
+                              //redirect to
+                              context.go('/create');
+                            },
+                            child: const Text(
+                              'Create Pizza',
+                              style: TextStyle(
+                                // fontSize: 20,
+                                color: Colors.grey,
+                                // fontWeight: FontWeight.w600,
                               ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Icon(
-                                CupertinoIcons.arrow_right_to_line,
-                              )
-                            ],
+                            ),
                           ),
-                        ),
-                ],
+                      ],
+                    ),
+                    InkWell(
+                            onTap: () {
+                              //onclick do this:
+                              context.read<SignInBloc>().add(SignOutRequired());
+                            },
+                            splashColor: Colors.red,
+                            child: const Row(
+                              children: [
+                                Text(
+                                  'Logout',
+                                  style: TextStyle(
+                                    // fontSize: 20,
+                                    // color: Colors.grey,
+                                    // fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Icon(
+                                  CupertinoIcons.arrow_right_to_line,
+                                )
+                              ],
+                            ),
+                          ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: widget.child
-            ),
-        ],
+            Expanded(
+              child: widget.child
+              ),
+          ],
+        ),
       ),
     );
   }
