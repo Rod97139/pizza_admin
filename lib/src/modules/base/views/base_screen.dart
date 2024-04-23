71ba0cf38c2f47f2199efdb62ce7b9c527746beb
Rod97139 <fayaflame@gmail.com>
2024-04-23 09:09:10 +0200

    logout fonctionnal

--- lib/src/modules/base/views/base_screen.dart
+++ lib/src/modules/base/views/base_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pizza_admin/src/modules/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 class BaseScreen extends StatefulWidget {
   final Widget child;
   const BaseScreen(this.child, {super.key});
@@ -21,35 +24,66 @@ class _BaseScreenState extends State<BaseScreen> {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30.0),
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  InkWell(
-                    onTap: () {
-                      context.go('/home');
-                    },
-                    child: const Text(
-                      'Pizza Admin',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 20,),
-                    InkWell(
-                      onTap: () {
-                        //redirect to
-                        context.go('/create');
-                      },
-                      child: const Text(
-                        'Create Pizza',
-                        style: TextStyle(
-                          // fontSize: 20,
-                          fontWeight: FontWeight.w600,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          context.go('/home');
+                        },
+                        child: const Text(
+                          'Pizza Admin',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
+                      const SizedBox(
+                        width: 20,),
+                        InkWell(
+                          onTap: () {
+                            //redirect to
+                            context.go('/create');
+                          },
+                          child: const Text(
+                            'Create Pizza',
+                            style: TextStyle(
+                              // fontSize: 20,
+                              color: Colors.grey,
+                              // fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                  InkWell(
+                          onTap: () {
+                            //onclick do this:
+                            context.read<SignInBloc>().add(SignOutRequired());
+                          },
+                          splashColor: Colors.red,
+                          child: const Row(
+                            children: [
+                              Text(
+                                'Logout',
+                                style: TextStyle(
+                                  // fontSize: 20,
+                                  // color: Colors.grey,
+                                  // fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                CupertinoIcons.arrow_right_to_line,
+                              )
+                            ],
+                          ),
+                        ),
                 ],
               ),
             ),
--- lib/src/modules/create_pizza/view/create_pizza_screen.dart
+++ lib/src/modules/create_pizza/view/create_pizza_screen.dart
@@ -9,20 +9,7 @@ class CreatePizzaScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-                
-            ),
-          ),
-          Expanded(
-            child: Container(
-            
-            ),
-          )
-        ] 
-        ),
+      
     );
   }
 }
